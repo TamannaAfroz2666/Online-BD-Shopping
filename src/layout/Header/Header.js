6dd3427119d279c10f4dea407a20fa7cfd41123f
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-07 10:50:07 +0600

    add logo

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,11 +1,29 @@
 import React from 'react';
 import './Header.css';
+import img from '../../assets/Images/logoTop.jpg';
+import logo from '../../assets/logo_01_AI_png.png';
 
 const Header = () => {
     return (
-        <div className='practice'>
-            <h1 className='practicehelo'>hhhh</h1>
-            
+        <div className='headerMain'>
+            <div className="subHeaderMain">
+                <div className="headerContainer">
+                    <div className="logoPart">
+                        <div className="logo">
+                            <img src={logo} alt="not found" />
+                        </div>
+                    </div>
+                    <div className="searchPart">
+
+                    </div>
+                    <div className="activitiesPart">
+
+                    </div>
+                </div>
+
+            </div>
+
+
         </div>
     );
 };
