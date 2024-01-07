5bbd513f8d91f2c9f0bfb7afa5f35c7a509acd25
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-07 12:28:10 +0600

    wishlist done

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -3,11 +3,12 @@ import './Header.css';
 import logo from '../../assets/logo_01_AI_png.png';
 import { LiaSearchSolid } from "react-icons/lia";
 import { RxCross2 } from "react-icons/rx";
+import { FaHeart } from "react-icons/fa";
 
 const Header = () => {
     // const [inputClicked, setInputClicked] = useState(false);
     const [searchText, setSearchText] = useState('')
-    const searchHandler = (e) =>{
+    const searchHandler = (e) => {
         setSearchText(e.target.value);
         console.log('hh');
         // setInputClicked(true);
@@ -27,34 +28,50 @@ const Header = () => {
                             </a>
                         </div>
                     </div>
-                    <div className="searchPart">
-                        <div className="searchField">
-                            <input
-                                type="text"
-                                className='searchBar'
-                                id='searchBar'
-                                name='search'
-                                placeholder='Search Product...' autoComplete='off'
-                                onChange={searchHandler}
-                                value={searchText}
-                            />
-                            {
-                                searchText && (
-                                    <button className='crossBtn' onClick={clearHandle}><RxCross2 size={20} /></button>
+                    <form action="">
+                        <div className="searchPart">
+                            <div className="searchField">
+                                <input
+                                    type="text"
+                                    className='searchBar'
+                                    id='searchBar'
+                                    name='search'
+                                    placeholder='Search Product...' autoComplete='off'
+                                    onChange={searchHandler}
+                                    value={searchText}
+                                />
+                                {
+                                    searchText && (
+                                        <button className='crossBtn' onClick={clearHandle}><RxCross2 size={20} /></button>
 
-                                )
-                            }
-                           
+                                    )
+                                }
 
 
+
+                            </div>
+                            <div className="searchBtn">
+                                <button className='btnSearchHeader'><LiaSearchSolid size={40} color='white' /></button>
+                            </div>
                         </div>
-                        <div className="searchBtn">
-                            <button className='btnSearchHeader'><LiaSearchSolid size={40} color='white' /></button>
-
-
-                        </div>
-                    </div>
+                    </form>
                     <div className="activitiesPart">
+                        <div className="wishlistHeader">
+                            <div className="wishIconSec">
+                           
+                            <FaHeart color='green' size={35} />
+                            <span className='wishlistItem'>10</span>
+                            </div>
+                           
+                            <p className='wishlistHeaderTitle'>Wishlist</p>
+
+                        </div>
+                        <div className="cartHeader">
+
+                        </div>
+                        <div className="profileHeader">
+
+                        </div>
 
                     </div>
                 </div>
