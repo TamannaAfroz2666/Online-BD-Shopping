87616eabffb6d831d2d74745bb68b527213b10c7
TamannaAfroz2666 <afroztamanna66@gmail.com>
2024-01-07 11:37:26 +0600

    search bar create

--- src/layout/Header/Header.js
+++ src/layout/Header/Header.js
@@ -1,20 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Header.css';
-import img from '../../assets/Images/logoTop.jpg';
 import logo from '../../assets/logo_01_AI_png.png';
+import { LiaSearchSolid } from "react-icons/lia";
+import { RxCross2 } from "react-icons/rx";
 
 const Header = () => {
+    const [inputClicked, setInputClicked] = useState(false);
+    const searchHandler = () =>{
+        console.log('hh');
+        setInputClicked(true);
+    }
     return (
         <div className='headerMain'>
             <div className="subHeaderMain">
                 <div className="headerContainer">
                     <div className="logoPart">
                         <div className="logo">
-                            <img src={logo} alt="not found" />
+                            <a href="/online-bd-shopping">
+                                <img src={logo} alt="not found" />
+                            </a>
                         </div>
                     </div>
                     <div className="searchPart">
+                        <div className="searchField">
+                            <input
+                                type="text"
+                                className='searchBar'
+                                id='searchBar'
+                                name='search'
+                                placeholder='Search Product...' autoComplete='off'
+                                onClick={searchHandler}
+                            />
+                            {
+                                inputClicked && (
+                                    <button className='crossBtn'><RxCross2 size={20} /></button>
 
+                                )
+                            }
+                           
+
+
+                        </div>
+                        <div className="searchBtn">
+                            <button className='btnSearchHeader'><LiaSearchSolid size={40} color='white' /></button>
+
+
+                        </div>
                     </div>
                     <div className="activitiesPart">
 
